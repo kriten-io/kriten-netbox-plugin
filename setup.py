b7ffda3cd0aac2a73f4e9744c7247e66eecfe978
beertwanger <steve@evolvere-tech.co.uk>
2024-09-17 14:16:59 +0100

    Added MANIFEST.in to allow packaging with setup.py to include the template files.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='kriten-netbox',
-    version='0.1.1',
+    version='0.1.2',
     description='Configure Kriten and run fobs jobs from NetBox',
     install_requires=[],
     packages=find_packages(),
